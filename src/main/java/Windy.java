8602a22369951c809b20b4245717b3e50c057462
little-zhenyao <zhenyao840@gmail.com>
2026-08-16 18:32:02 +0800

    Add add and list function

--- src/main/java/Windy.java
+++ src/main/java/Windy.java
@@ -6,51 +6,79 @@ import java.util.Scanner;
 public class Windy {
 
     private static final String NAME = "Windy";
+    private static final int MAX_TASKS = 100;
+    private final String[] tasks;
+    private int sizeOfTasks;
 
-    private static void greet() {
+    private Windy() {
+        tasks = new String[MAX_TASKS];
+        sizeOfTasks = 0;
+    }
+
+    private void greet() {
         printSeparator();
+
         printWindyBanner();
-        System.out.println("Hello! I'm " + NAME + ".");
-        System.out.println("What can I do for you?");
+        System.out.println("     Hello! I'm " + NAME + ".");
+        System.out.println("     What can I do for you?");
         printSeparator();
     }
 
-    private static void sayBye() {
-        System.out.println("Bye. Hope to see you again soon!");
+    private void sayBye() {
+        System.out.println("     Bye. Hope to see you again soon!");
         printSeparator();
     }
 
     /**
-     * Repeat the words that user inputs until user inputs the word "bye"
+     * If user's input is "list", list all the task
+     * If user's input is "bye", exit
+     * Otherwise, add a task named as user's input to the array
      */
-    private static void echo() {
+    private void runCommandLoop() {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
             printSeparator();
-            System.out.println(input);
+            if (input.equals("list")) {
+                this.listTasks();
+            } else {
+                this.addTask(input);
+            }
             printSeparator();
             input = scanner.nextLine();
         }
         printSeparator();
     }
 
-    private static void printWindyBanner() {
-        String banner = "__        ___           _       \n"
-                + "\\ \\      / (_)_ __   __| |_   _ \n"
-                + " \\ \\ /\\ / /| | '_ \\ / _` | | | |\n"
-                + "  \\ V  V / | | | | | (_| | |_| |\n"
-                + "   \\_/\\_/  |_|_| |_|\\__,_|\\__, |\n"
-                + "                          |___/ \n";
+    private void addTask(String input) {
+        System.out.println("     add: " + input);
+        this.tasks[sizeOfTasks] = input;
+        sizeOfTasks++;
+    }
+
+    private void listTasks() {
+        for (int i = 0; i < sizeOfTasks; i++) {
+            System.out.println("     " + (i + 1) + ". " + tasks[i]);
+        }
+    }
+
+    private void printWindyBanner() {
+        String banner = "     __        ___           _       \n"
+                + "     \\ \\      / (_)_ __   __| |_   _ \n"
+                + "      \\ \\ /\\ / /| | '_ \\ / _` | | | |\n"
+                + "       \\ V  V / | | | | | (_| | |_| |\n"
+                + "        \\_/\\_/  |_|_| |_|\\__,_|\\__, |\n"
+                + "                               |___/ \n";
         System.out.print(banner);
     }
-    private static void printSeparator() {
-        System.out.println("____________________________________________________________");
+    private void printSeparator() {
+        System.out.println("    ____________________________________________________________");
     }
 
     public static void main(String[] args) {
-        greet();
-        echo();
-        sayBye();
+        Windy windy = new Windy();
+        windy.greet();
+        windy.runCommandLoop();
+        windy.sayBye();
     }
 }
