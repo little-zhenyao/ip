e8453bae395d06706cafe42e9159dd0b9b19dfb9
little-zhenyao <zhenyao840@gmail.com>
2026-08-18 11:57:14 +0800

    Add delete function

--- src/main/java/Windy.java
+++ src/main/java/Windy.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -6,13 +7,10 @@ import java.util.Scanner;
 public class Windy {
 
     private static final String NAME = "Windy";
-    private static final int MAX_TASKS = 100;
-    private final Task[] tasks;
-    private int sizeOfTasks;
+    private final ArrayList<Task> tasks;
 
     private Windy() {
-        tasks = new Task[MAX_TASKS];
-        sizeOfTasks = 0;
+        tasks = new ArrayList<>();
     }
 
     private void greet() {
@@ -32,7 +30,7 @@ public class Windy {
     /**
      * If user's input is "list", list all the task
      * If user's input is "bye", exit
-     * Otherwise, add a task named as user's input to the array
+     * Otherwise, add a task named as user's input to the list
      *      the type of task is defined by user's first input word
      */
     private void runCommandLoop() {
@@ -54,6 +52,11 @@ public class Windy {
                         throw new InvalidInputFormatException("     Invalid format. Please use: mark TASK_NUMBER");
                     }
                     this.markTask(command[1], mark);
+                } else if (command[0].equals("delete")) {
+                    if (command.length != 2) {
+                        throw new InvalidInputFormatException("     Invalid format. Please use: delete TASK_NUMBER");
+                    }
+                    this.deleteTask(command[1]);
                 } else {
                     this.addTask(input);
                 }
@@ -66,28 +69,42 @@ public class Windy {
         printSeparator();
     }
 
-    private void markTask(String taskNumber, boolean mark) throws InvalidInputFormatException {
+    private int getValidTaskIndex(String taskNumber) throws InvalidInputFormatException {
         int num;
         try {
             num = Integer.parseInt(taskNumber) - 1;
         } catch (NumberFormatException e) {
             throw new InvalidInputFormatException("     The number must be a positive integer");
         }
-        if (num < 0 || num >= sizeOfTasks) {
-            if (sizeOfTasks == 0) {
+        if (num < 0 || num >= tasks.size()) {
+            if (tasks.isEmpty()) {
                 throw new InvalidInputFormatException("     There are no tasks in the list.");
             } else {
                 throw new InvalidInputFormatException("     Invalid number of task, " +
-                        "please try the number between 1 and " + sizeOfTasks);
+                        "please try the number between 1 and " + tasks.size());
             }
         }
-        tasks[num].setDone(mark);
+        return num;
+    }
+
+    private void deleteTask(String taskNumber) throws InvalidInputFormatException {
+        int num =  getValidTaskIndex(taskNumber);
+
+        System.out.println("     Noted. I've removed this task:");
+        System.out.println("       " + tasks.get(num));
+        tasks.remove(num);
+        System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
+    }
+
+    private void markTask(String taskNumber, boolean mark) throws InvalidInputFormatException {
+        int num = getValidTaskIndex(taskNumber);
+        tasks.get(num).setDone(mark);
         if (mark) {
             System.out.println("     Nice! I've marked this task as done:");
         } else {
             System.out.println("     OK, I've marked this task as not done yet:");
         }
-        System.out.println("       " + tasks[num].toString());
+        System.out.println("       " + tasks.get(num));
     }
 
     private void addTask(String input) throws InvalidInputFormatException {
@@ -99,20 +116,17 @@ public class Windy {
         if (command.length == 1) {
             throw new InvalidInputFormatException("     The description of task cannot be empty");
         }
-        if (sizeOfTasks >=  MAX_TASKS) {
-            throw new InvalidInputFormatException("     The task list is full");
-        }
         String details = input.substring(command[0].length() + 1).trim();
         switch (command[0]) {
             case "todo" -> {
-                this.tasks[sizeOfTasks] = new Todo(details, false);
+                this.tasks.add(new Todo(details, false));
             }
             case "deadline" -> {
                 String[] parts = details.split("\\s+/by\\s+", 2);
                 if (parts.length != 2) {
                     throw new InvalidInputFormatException("     The format of deadline is wrong. Please use description /by date");
                 }
-                this.tasks[sizeOfTasks] = new Deadline(parts[0], false, parts[1]);
+                this.tasks.add(new Deadline(parts[0], false, parts[1]));
             }
             case "event" -> {
                 String[] fromParts = details.split("\\s+/from\\s+", 2);
@@ -127,22 +141,21 @@ public class Windy {
                 }
                 String from = timeParts[0].trim();
                 String to = timeParts[1].trim();
-                this.tasks[sizeOfTasks] = new Event(name, false, from, to);
+                this.tasks.add(new Event(name, false, from, to));
             }
             default -> {
                 throw new InvalidInputFormatException("     Invalid command, please try another one");
             }
         }
-        sizeOfTasks++;
         System.out.println("     Got it. I've added this task:");
-        System.out.println("       " +  tasks[sizeOfTasks - 1].toString());
-        System.out.println("     Now you have " + sizeOfTasks + " tasks in the list.");
+        System.out.println("       " + tasks.get(tasks.size() - 1));
+        System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
     }
 
     private void listTasks() {
         System.out.println("     Here are the tasks in your list:");
-        for (int i = 0; i < sizeOfTasks; i++) {
-            System.out.println("     " + (i + 1) + "." + tasks[i].toString());
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println("     " + (i + 1) + "." + tasks.get(i));
         }
     }
 
